1c9ab86874aa8e174d65310416a304596e41aeda
ajlopez <webmaster@ajlopez.com>
2013-04-07 06:52:24 -0300

    GetGenres first test

---
+++ Src/MyLibrary.Web.Tests/Controllers/GenreControllerTests.cs
@@ -0,0 +1,39 @@
+﻿namespace MyLibrary.Web.Tests.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Web.Mvc;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using MyLibrary.Web.Controllers;
+    using MyLibrary.Web.Models;
+
+    [TestClass]
+    public class GenreControllerTests
+    {
+        [TestMethod]
+        public void GetGenres()
+        {
+            IList<Genre> genres = new List<Genre>()
+            {
+                new Genre() { Name = "Mathematics" },
+                new Genre() { Name = "Physics" },
+                new Genre() { Name = "Biology" },
+                new Genre() { Name = "Literature" }
+            };
+
+            GenreController controller = new GenreController(genres);
+
+            ActionResult result = controller.Index();
+
+            Assert.IsNotNull(result);
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+            ViewResult viewResult = (ViewResult)result;
+
+            Assert.IsInstanceOfType(viewResult.ViewData.Model, typeof(IList<Genre>));
+            Assert.AreSame(genres, viewResult.ViewData.Model);
+        }
+    }
+}
---
+++ Src/MyLibrary.Web/Controllers/GenreController.cs
@@ -0,0 +1,28 @@
+﻿namespace MyLibrary.Web.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Web;
+    using System.Web.Mvc;
+    using MyLibrary.Web.Models;
+
+    public class GenreController : Controller
+    {
+        private IList<Genre> genres;
+
+        public GenreController()
+        {
+        }
+
+        public GenreController(IList<Genre> genres)
+        {
+            this.genres = genres;
+        }
+
+        public ActionResult Index()
+        {
+            return this.View(this.genres);
+        }
+    }
+}
